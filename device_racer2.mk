24e63159e38b677bea88a3e5565dd948e2dc1ed8
Luca Weiss <luca@z3ntu.xyz>
2011-02-07 12:32:55 -0800

    Initial commit for racer2 based on sebastian404/android_device_zte_mooncake

---
+++ device_racer2.mk
@@ -0,0 +1,26 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/zte/racer2/overlay
+
+
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := device/zte/racer2/recovery_kernel_gen1
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := generic_racer2
+PRODUCT_DEVICE := racer2
+
+pathmap_INCL := recovery:device/zte/racer2/recovery
+
+
