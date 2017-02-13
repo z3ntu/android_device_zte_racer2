667653f1434ed3548988f63014e596dc312b7f56
Luca Weiss <luca@z3ntu.xyz>
2017-02-13 22:18:22 +0100

    Comment stuff out and change sizes to other values???

--- device_racer2.mk
+++ device_racer2.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/zte/racer2/overlay
+#DEVICE_PACKAGE_OVERLAYS += device/zte/racer2/overlay
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/zte/racer2/prebuilts/kernel
@@ -20,6 +20,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := generic_racer2
 PRODUCT_DEVICE := racer2
 
-pathmap_INCL := recovery:device/zte/racer2/recovery
+#pathmap_INCL := recovery:device/zte/racer2/recovery
 
 
